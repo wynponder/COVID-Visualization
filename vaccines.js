ce444eba5fcb40817c093bb038b5be19f1f6a927
Wyn Ponder <wponder@captechventures.com>
2021-06-01 16:00:09 -0400

    Updating to use JQuery instead of JS to render HTML elements

--- vaccines.js
+++ vaccines.js
@@ -14,43 +14,29 @@ $(document).ready(function () {
     $.getJSON("https://covid-api.mmediagroup.fr/v1/cases?country=US",
         function (data) {
             var confirmedCasesUS = data["All"]["confirmed"];
-            document.getElementById("totalCasesUS").innerHTML = numberWithCommas(confirmedCasesUS);
+            $('#totalCasesUS').html(numberWithCommas(confirmedCasesUS));
 
             n = new Date();
             y = n.getFullYear();
             m = n.getMonth() + 1;
             d = n.getDate();
-            var todayDate = document.getElementById("todayDate").innerHTML = m + "/" + d + "/" + y;
-
+            var todayDate = $('#todayDate').html(m + "/" + d + "/" + y);
         })
 });
-
-/*var items = [];
-var confirmedCases;
-
-$(document).ready(function () {
-    $.getJSON(url,
-        function (data) {
-            confirmedCases = $.each(data, function (key, value) {
-                return data.;
-            });
-            console.log(confirmedCases);
-        })
-})*/
-
 
 //top right component - complete vaccinations
 $(document).ready(function () {
     $.getJSON("https://covid-api.mmediagroup.fr/v1/vaccines?country=US",
         function (data) {
             var peopleFullyVaccinated = data["All"]["people_vaccinated"];
-            document.getElementById("usaPeople").innerHTML = numberWithCommas(peopleFullyVaccinated);
+            $('#usaPeople').html(numberWithCommas(peopleFullyVaccinated));
 
             n = new Date();
             y = n.getFullYear();
             m = n.getMonth() + 1;
             d = n.getDate();
-            var todayDate = document.getElementById("todayDateTwo").innerHTML = m + "/" + d + "/" + y;
+
+            var todayDate = $('#todayDateTwo').html(m + "/" + d + "/" + y);
 
         })
 });
@@ -60,13 +46,14 @@ $(document).ready(function () {
     $.getJSON("https://covid-api.mmediagroup.fr/v1/vaccines?country=US",
         function (data) {
             var partiallyVaccinated = data["All"]["people_partially_vaccinated"];
-            document.getElementById("partialUS").innerHTML = numberWithCommas(partiallyVaccinated);
+            $('#partialUS').html(numberWithCommas(partiallyVaccinated));
+
 
             n = new Date();
             y = n.getFullYear();
             m = n.getMonth() + 1;
             d = n.getDate();
-            var todayDate = document.getElementById("todayDateThree").innerHTML = m + "/" + d + "/" + y;
+            var todayDate = $('#todayDateThree').html(m + "/" + d + "/" + y);
 
         })
 });
@@ -76,7 +63,7 @@ $(document).ready(function () {
     $.getJSON("https://covid-api.mmediagroup.fr/v1/vaccines?country=US",
         function (data) {
             var administeredShots = data["All"]["administered"];
-            document.getElementById("admin").innerHTML = numberWithCommas(administeredShots);
+            $('#admin').html(numberWithCommas(administeredShots));
         })
 });
 
@@ -85,40 +72,42 @@ $(document).ready(function () {
     $.getJSON("https://covid-api.mmediagroup.fr/v1/cases?country=US",
         function (data) {
             var sortable = [];
-            /*create variable called ignored states - actual check here within the for loop would be if state is not in ignored states, push*/
+            //filtering out non-states from new array
             var ignoredStates = [
                 "All",
                 "Grand Princess"
             ];
-            //console.log(data);
             for (var state in data) {
-                //if ignored states includes state, it would just push all and GP
                 if (!ignoredStates.includes(state)) {
                     sortable.push([state, data[state].confirmed])
                 };
             }
 
-
             var sorted = sortable.sort(function (a, b) {
                 return a[1] < b[1] ? 1 : -1;
             })
 
-
+            //pulling the top three states from the new array
             var firstState = sorted[0];
             var secondState = sorted[1];
             var thirdState = sorted[2];
 
-            //document.getElementById("firstState").innerHTML = firstState.join(" ");
-            document.getElementById("firstState").innerHTML = firstState[0] + ": " + numberWithCommas(firstState[1]);
-            document.getElementById("secondState").innerHTML = secondState[0] + ": " + numberWithCommas(secondState[1]);
-            document.getElementById("thirdState").innerHTML = thirdState[0] + ": " + numberWithCommas(thirdState[1]);
+            //adding commas to the number of cases for each of the top three states
+            var firstStateCommas = firstState[0] + ": " + numberWithCommas(firstState[1]);
+            var secondStateCommas = secondState[0] + ": " + numberWithCommas(secondState[1]);
+            var thirdStateCommas = thirdState[0] + ": " + numberWithCommas(thirdState[1]);
 
+            //using JQuery to show the results of the top three states
+            $('#firstState').html(firstStateCommas);
+            $('#secondState').html(secondStateCommas);
+            $('#thirdState').html(thirdStateCommas);
 
+            //showing today's date to update the data
             n = new Date();
             y = n.getFullYear();
             m = n.getMonth() + 1;
             d = n.getDate();
-            var todayDate = document.getElementById("todayDateFour").innerHTML = m + "/" + d + "/" + y;
+            var todayDate = $('#todayDateFour').html(m + "/" + d + "/" + y);
 
         })
 });
