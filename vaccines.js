2e308d05c76f61fabcd5b65dccc38b90d0149192
Wyn Ponder <wponder@captechventures.com>
2021-06-11 14:46:37 -0400

    reverting back to previous change

--- vaccines.js
+++ vaccines.js
@@ -9,89 +9,105 @@ function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
-//Total US Cases
+//top left component - confirmed US Cases
 $(document).ready(function () {
     $.getJSON("https://covid-api.mmediagroup.fr/v1/cases?country=US",
         function (data) {
             var confirmedCasesUS = data["All"]["confirmed"];
-            document.getElementById("totalCasesUS").innerHTML = numberWithCommas(confirmedCasesUS);
+            $('#totalCasesUS').html(numberWithCommas(confirmedCasesUS));
 
             n = new Date();
             y = n.getFullYear();
             m = n.getMonth() + 1;
             d = n.getDate();
-            var todayDate = document.getElementById("todayDate").innerHTML = m + "/" + d + "/" + y;
-
+            var todayDate = $('#todayDate').html(m + "/" + d + "/" + y);
         })
 });
 
-/*var items = [];
-var confirmedCases;
-
+//top right component - complete vaccinations
 $(document).ready(function () {
-    $.getJSON(url,
-        function (data) {
-            confirmedCases = $.each(data, function (key, value) {
-                return data.;
-            });
-            console.log(confirmedCases);
-        })
-})*/
-
-
-var usVaccineData = {
-    "url": "https://covid-api.mmediagroup.fr/v1/vaccines?country=United%20States",
-    "method": "GET",
-    "timeout": 0,
-};
-
-$(document).ready(function () {
-    $.getJSON("https://covid-api.mmediagroup.fr/v1/vaccines?country=United%20States",
+    $.getJSON("https://covid-api.mmediagroup.fr/v1/vaccines?country=US",
         function (data) {
             var peopleFullyVaccinated = data["All"]["people_vaccinated"];
-            document.getElementById("usaPeople").innerHTML = numberWithCommas(peopleFullyVaccinated);
+            $('#usaPeople').html(numberWithCommas(peopleFullyVaccinated));
 
             n = new Date();
             y = n.getFullYear();
             m = n.getMonth() + 1;
             d = n.getDate();
-            var todayDate = document.getElementById("todayDateTwo").innerHTML = m + "/" + d + "/" + y;
+
+            var todayDate = $('#todayDateTwo').html(m + "/" + d + "/" + y);
 
         })
 });
 
+//bottom left component - partial vaccinations
 $(document).ready(function () {
-    $.getJSON("https://covid-api.mmediagroup.fr/v1/vaccines?country=United%20States",
+    $.getJSON("https://covid-api.mmediagroup.fr/v1/vaccines?country=US",
         function (data) {
             var partiallyVaccinated = data["All"]["people_partially_vaccinated"];
-            document.getElementById("partialUS").innerHTML = numberWithCommas(partiallyVaccinated);
+            $('#partialUS').html(numberWithCommas(partiallyVaccinated));
+
 
             n = new Date();
             y = n.getFullYear();
             m = n.getMonth() + 1;
             d = n.getDate();
-            var todayDate = document.getElementById("todayDateThree").innerHTML = m + "/" + d + "/" + y;
+            var todayDate = $('#todayDateThree').html(m + "/" + d + "/" + y);
 
         })
 });
 
+//center component - total vaccinations: partial + complete
 $(document).ready(function () {
-    $.getJSON("https://covid-api.mmediagroup.fr/v1/vaccines?country=United%20States",
+    $.getJSON("https://covid-api.mmediagroup.fr/v1/vaccines?country=US",
         function (data) {
             var administeredShots = data["All"]["administered"];
-            document.getElementById("admin").innerHTML = numberWithCommas(administeredShots);
+            $('#admin').html(numberWithCommas(administeredShots));
         })
 });
 
-/*$(document).ready(function () {
-    $.getJSON("https://covid-api.mmediagroup.fr/v1/cases?ab=US&status=confirmed",
+//bottom right component - top three states
+$(document).ready(function () {
+    $.getJSON("https://covid-api.mmediagroup.fr/v1/cases?country=US",
         function (data) {
-            var top5 = function sortByKeyDesc(array, confirmed) {
-                return array.sort(function (a, b) {
-                    var x = a["confirmed"]; var y = b["confirmed"];
-                    return ((x > y) ? -1 : ((x < y) ? 1 : 0));
-                });
+            var sortable = [];
+            //filtering out non-states from new array
+            var ignoredStates = [
+                "All",
+                "Grand Princess"
+            ];
+            for (var state in data) {
+                if (!ignoredStates.includes(state)) {
+                    sortable.push([state, data[state].confirmed])
+                };
             }
-            console.log(top5); 
+
+            var sorted = sortable.sort(function (a, b) {
+                return a[1] < b[1] ? 1 : -1;
+            })
+
+            //pulling the top three states from the new array
+            var firstState = sorted[0];
+            var secondState = sorted[1];
+            var thirdState = sorted[2];
+
+            //adding commas to the number of cases for each of the top three states
+            var firstStateCommas = firstState[0] + ": " + numberWithCommas(firstState[1]);
+            var secondStateCommas = secondState[0] + ": " + numberWithCommas(secondState[1]);
+            var thirdStateCommas = thirdState[0] + ": " + numberWithCommas(thirdState[1]);
+
+            //using JQuery to show the results of the top three states
+            $('#firstState').html(firstStateCommas);
+            $('#secondState').html(secondStateCommas);
+            $('#thirdState').html(thirdStateCommas);
+
+            //showing today's date to update the data
+            n = new Date();
+            y = n.getFullYear();
+            m = n.getMonth() + 1;
+            d = n.getDate();
+            var todayDate = $('#todayDateFour').html(m + "/" + d + "/" + y);
+
         })
-});*/
+});
